e8520d5ca53953a7a35566ce36ad92f59be4e161
David <davidparraausina@gmail.com>
2024-01-22 10:13:09 +0000

    Remove guild tasks code from core

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -805,7 +805,6 @@ class ChatHandler
         bool HandlePlayerbotCommand(char* args);
         bool HandleRandomPlayerbotCommand(char* args);
         bool HandleAhBotCommand(char* args);
-        bool HandleGuildTaskCommand(char* args);
 #endif
 
         bool HandleArenaFlushPointsCommand(char* args);
